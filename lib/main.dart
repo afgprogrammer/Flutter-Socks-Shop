112da216c8921d7042b646ee9b5b97c9be16e543
Mohammad Rahmani <rto1680@gmail.com>
2020-02-19 22:16:58 +0430

    Make home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,205 @@
+import 'package:day20_3_2/Animations/FadeAnimation.dart';
+import 'package:day20_3_2/ViewSocks.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:page_transition/page_transition.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    )
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            FadeAnimation(1, Container(
+              height: 300,
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 25, right: 25, top: 60),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50.0), 
+                  bottomLeft: Radius.circular(50.0)
+                ),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  colors: [
+                    Color.fromRGBO(255, 250, 182, 1),
+                    Color.fromRGBO(255, 239, 78, 1)
+                  ]
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(1, Image.asset('assets/menu.png', width: 20,)),
+                  SizedBox(height: 15,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: FadeAnimation(1.2, Text('Best Online \nSocks Collection', style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(97, 90, 90, 1)
+                        )),),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: FadeAnimation(1.3, Image.asset('assets/header-socks.png')),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )),
+            Transform.translate(
+              offset: Offset(0, -35),
+              child: FadeAnimation(1.2, Container(
+                height: 60,
+                padding: EdgeInsets.only(left: 20, top: 8),
+                margin: EdgeInsets.symmetric(horizontal: 25),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[350],
+                      blurRadius: 20.0,
+                      offset: Offset(0, 10.0)
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Colors.white
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.search, color: Colors.black, size: 20.0,),
+                    border: InputBorder.none,
+                    hintText: 'Search'
+                  ),
+                ),
+              )),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      FadeAnimation(1.2, Text('Choose \na category', style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Color.fromRGBO(97, 90, 90, 1)
+                      )),),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            FadeAnimation(1.2, FlatButton(
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                              padding: EdgeInsets.all(10),
+                              onPressed: () {},
+                              color: Color.fromRGBO(251, 53, 105, 0.1),
+                              child: Text('Adult', style: TextStyle(
+                                color: Color.fromRGBO(251, 53, 105, 1),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold
+                              )),
+                            )),
+                            SizedBox(width: 20.0,),
+                            FadeAnimation(1.3, FlatButton(
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                              padding: EdgeInsets.all(10),
+                              onPressed: () {},
+                              color: Color.fromRGBO(97, 90, 90, 0.1),
+                              child: Text('Children', style: TextStyle(
+                                color: Color.fromRGBO(97, 90, 90, 0.6),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold
+                              )),
+                            )),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
+            SizedBox(height: 30.0,),
+            Container(
+              height: 280,
+              width: double.infinity,
+              child: ListView(
+                padding: EdgeInsets.only(bottom: 20, left: 20),
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  FadeAnimation(1.3, makeCard(
+                    context: context,
+                    startColor: Color.fromRGBO(251, 121, 155, 1),
+                    endColor: Color.fromRGBO(251, 53, 105, 1),
+                    image: 'assets/socks-one.png'
+                  )),
+                  FadeAnimation(1.4, makeCard(
+                    context: context,
+                    startColor: Color.fromRGBO(203, 251, 255, 1),
+                    endColor: Color.fromRGBO(81, 223, 234, 1),
+                    image: 'assets/socks-two.png'
+                  )),
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget makeCard({context, startColor, endColor, image}) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: ViewSocks()));
+      },
+      child: AspectRatio(
+        aspectRatio: 4/5,
+        child: Container(
+          margin: EdgeInsets.only(right: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(13.0),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              colors: [
+                startColor,
+                endColor
+              ],
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[350],
+                blurRadius: 10,
+                offset: Offset(5, 10)
+              )
+            ]
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(50),
+            child: Center(
+              child: Image.asset(image),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
